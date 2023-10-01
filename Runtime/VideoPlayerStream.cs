11fa0a3548846d23fbc51c4d19e6e8972c982c5f
meangpu <51825872+meangpu@users.noreply.github.com>
2023-10-01 16:53:10 +0700

    Update VideoPlayerStream.cs

--- Runtime/VideoPlayerStream.cs
+++ Runtime/VideoPlayerStream.cs
@@ -1,4 +1,3 @@
-using UnityEditor;
 using UnityEngine;
 
 namespace Meangpu.Video
@@ -6,14 +5,12 @@ namespace Meangpu.Video
     public class VideoPlayerStream : VideoPlayerBase
     {
         [Header("Drag video from 'StreamingAssets' HERE")]
-#if UNITY_EDITOR
-        [SerializeField] DefaultAsset _video;
-#endif
+        [SerializeField] string _videoName;
         [SerializeField] string _videoExtension = ".mp4";
 
         protected override void InitVideoPlayer()
         {
-            _videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, _video.name + _videoExtension);
+            _videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, _videoName + _videoExtension);
             _rawImg.texture = _mainTexture;
             _videoPlayer.targetTexture = _mainTexture;
         }
